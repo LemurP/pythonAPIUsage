9aa78d819a2f08fb924ffcfdd6c00743fbc484f3
Peter Norstein <peter.norstein@gmail.com>
2021-03-28 17:39:24 +0200

    Improve naming

--- saveAstroImageOfTheDay.py
+++ saveAstroImageOfTheDay.py
@@ -2,8 +2,12 @@ import json
 
 import requests
 
-r = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-fileURL = json.JSONDecoder().decode(r.content.decode())['hdurl']
+response = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+json_decoder = json.JSONDecoder()
+response_content = response.content.decode()
+json_representation = json_decoder.decode(response_content)
+fileURL = json_representation['hdurl']
 r2 = requests.get(fileURL)
+
 with open("test-nasa.jpeg", "wb") as file:
     file.write(r2.content)
