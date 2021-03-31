8c76d25cb8fc2eeb4fa70ce76af8dd9a9a33a18e
Peter Norstein <peter.norstein@gmail.com>
2021-03-31 18:14:51 +0200

    Use known response to test

--- test/test_findNearEarthObjects.py
+++ test/test_findNearEarthObjects.py
@@ -4,10 +4,9 @@ from findNearEarthObjects import getFromAPIAndJsonify
 
 
 @pytest.mark.calls_real_api
-def test_():
+def test_known_result_from_neo_api_call():
     near_earth_objects = getFromAPIAndJsonify(
         'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-01-01&end_date=2021-01-01&api_key=DEMO_KEY')
     with open("test/test_neo_call_response.json", "r") as file:
         contents = file.read()
         assert str(near_earth_objects) == contents
-    pass
