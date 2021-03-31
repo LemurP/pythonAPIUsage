8986d8eca744a9463eb0fa4650ce7982ef6eb208
Peter Norstein <peter.norstein@gmail.com>
2021-03-31 19:31:32 +0200

    Add test for identifying the closest dangerous object

--- findNearEarthObjects.py
+++ findNearEarthObjects.py
@@ -3,13 +3,29 @@ import json
 import requests
 
 
-def getFromAPIAndJsonify(url):
+def getFromAPI(url):
     response = requests.get(url)
-    json_decoder = json.JSONDecoder()
-    response_content = response.content.decode()
-    return json_decoder.decode(response_content)
+    return response.content.decode()
+
+
+def jsonify(response_content):
+    return json.loads(response_content)
 
 
 if __name__ == "__main__":
-    near_earth_objects = getFromAPIAndJsonify('https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY')
+    near_earth_objects = jsonify(getFromAPI('https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY'))
     only_dangerous = near_earth_objects['near_earth_objects']
+
+
+def identify_nearest_dangerous_object(input_json):
+    days: dict = input_json['near_earth_objects']
+    result = None
+    result_distance = float("inf")
+    for neos in days.values():
+        for neo in neos:
+            if neo['is_potentially_hazardous_asteroid']:
+                if result_distance > float(neo['close_approach_data'][0]['miss_distance']['kilometers']):
+                    result = neo
+                    result_distance = float(neo['close_approach_data'][0]['miss_distance']['kilometers'])
+
+    return result
--- test/test_findNearEarthObjects.py
+++ test/test_findNearEarthObjects.py
@@ -1,12 +1,28 @@
 import pytest
 
-from findNearEarthObjects import getFromAPIAndJsonify
+import findNearEarthObjects
+from findNearEarthObjects import getFromAPI, jsonify
 
 
 @pytest.mark.calls_real_api
 def test_known_result_from_neo_api_call():
-    near_earth_objects = getFromAPIAndJsonify(
-        'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-01-01&end_date=2021-01-01&api_key=DEMO_KEY')
+    near_earth_objects = jsonify(getFromAPI(
+        'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-01-01&end_date=2021-01-01&api_key=DEMO_KEY'))
     with open("test/test_neo_call_response.json", "r") as file:
         contents = file.read()
         assert str(near_earth_objects) == contents
+
+
+@pytest.mark.calls_real_api
+def test_nearest_object_with_dangerous_tag_is_identified():
+    near_earth_objects = jsonify(getFromAPI(
+        'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-01-02&end_date=2021-01-02&api_key=DEMO_KEY'))
+    result = near_earth_objects
+
+    nearest_dangerous_object = findNearEarthObjects.identify_nearest_dangerous_object(result)
+    assert nearest_dangerous_object['id'] == '54095182'
+
+
+# Dummy test so pytest won't fail in pipeline
+def dummy_test():
+    pass
