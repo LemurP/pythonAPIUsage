5d5c5fc266a48d0db180e097900964f786a0d92e
Peter Norstein <peter.norstein@gmail.com>
2021-04-03 10:29:48 +0200

    Sort classes based on referenced in top class

--- src/models/near_earth_object.py
+++ src/models/near_earth_object.py
@@ -1,37 +1,29 @@
 from typing import List
 
 
-class EstimatedDiameter:
+class FeedAPIObject:
     def __init__(self, raw_json):
-        self.kilometers = raw_json['kilometers']
-        self.meters = raw_json['meters']
-        self.miles = raw_json['miles']
-        self.feet = raw_json['feet']
+        self.links = Links(raw_json['links'])
+        self.element_count: int = raw_json['element_count']
+        self.daily_near_earth_objects: List[Daily] = [Daily(day, daily_raw_json) for day, daily_raw_json in
+                                                      raw_json['near_earth_objects'].items()]
 
 
-class RelativeVelocity:
+class Links:
     def __init__(self, raw_json):
-        self.kilometers_per_second = raw_json['kilometers_per_second']
-        self.kilometers_per_hour = raw_json['kilometers_per_hour']
-        self.miles_per_hour = raw_json['miles_per_hour']
+        try:
+            self.next = raw_json['next']
+            self.prev = raw_json['prev']
+        except KeyError:
+            self.next = ""
+            self.prev = ""
+        self.self = raw_json['self']
 
 
-class MissDistance:
-    def __init__(self, raw_json):
-        self.astronomical = raw_json['astronomical']
-        self.lunar = raw_json['lunar']
-        self.kilometers: float = float(raw_json['kilometers'])
-        self.miles = raw_json['miles']
-
-
-class CloseApproachData:
-    def __init__(self, raw_json):
-        self.close_approach_date = raw_json['close_approach_date']
-        self.close_approach_date_full = raw_json['close_approach_date_full']
-        self.epoch_date_close_approach = raw_json['epoch_date_close_approach']
-        self.relative_velocity: RelativeVelocity = RelativeVelocity(raw_json['relative_velocity'])
-        self.miss_distance: MissDistance = MissDistance(raw_json['miss_distance'])
-        self.orbiting_body = raw_json['orbiting_body']
+class Daily:
+    def __init__(self, day, raw_json):
+        self.day: str = day
+        self.near_earth_objects: List[NearEarthObject] = [NearEarthObject(raw_json_neo) for raw_json_neo in raw_json]
 
 
 class NearEarthObject:
@@ -53,26 +45,34 @@ class NearEarthObject:
         return self.id
 
 
-class Daily:
-    def __init__(self, day, raw_json):
-        self.day: str = day
-        self.near_earth_objects: List[NearEarthObject] = [NearEarthObject(raw_json_neo) for raw_json_neo in raw_json]
-
-
-class FeedAPIObject:
+class EstimatedDiameter:
     def __init__(self, raw_json):
-        self.links = Links(raw_json['links'])
-        self.element_count: int = raw_json['element_count']
-        self.daily_near_earth_objects: List[Daily] = [Daily(day, daily_raw_json) for day, daily_raw_json in
-                                                      raw_json['near_earth_objects'].items()]
+        self.kilometers = raw_json['kilometers']
+        self.meters = raw_json['meters']
+        self.miles = raw_json['miles']
+        self.feet = raw_json['feet']
 
 
-class Links:
+class CloseApproachData:
     def __init__(self, raw_json):
-        try:
-            self.next = raw_json['next']
-            self.prev = raw_json['prev']
-        except KeyError:
-            self.next = ""
-            self.prev = ""
-        self.self = raw_json['self']
+        self.close_approach_date = raw_json['close_approach_date']
+        self.close_approach_date_full = raw_json['close_approach_date_full']
+        self.epoch_date_close_approach = raw_json['epoch_date_close_approach']
+        self.relative_velocity: RelativeVelocity = RelativeVelocity(raw_json['relative_velocity'])
+        self.miss_distance: MissDistance = MissDistance(raw_json['miss_distance'])
+        self.orbiting_body = raw_json['orbiting_body']
+
+
+class RelativeVelocity:
+    def __init__(self, raw_json):
+        self.kilometers_per_second = raw_json['kilometers_per_second']
+        self.kilometers_per_hour = raw_json['kilometers_per_hour']
+        self.miles_per_hour = raw_json['miles_per_hour']
+
+
+class MissDistance:
+    def __init__(self, raw_json):
+        self.astronomical = raw_json['astronomical']
+        self.lunar = raw_json['lunar']
+        self.kilometers: float = float(raw_json['kilometers'])
+        self.miles = raw_json['miles']
